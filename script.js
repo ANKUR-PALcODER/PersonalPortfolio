094c72455b6e1a8d1474de7507195d1ddea72341
Ankur <newstudent202223@gmail.com>
2023-08-28 20:21:43 +0530

    Removed Mouse Follower

--- script.js
+++ script.js
@@ -4,33 +4,33 @@ const scroll = new LocomotiveScroll({
 });
 
 // Function to implement the moving circle animation of the mouse circle
-function mouseCircle(xscale,yscale){
-    window.addEventListener('mousemove',(element)=>{
-        let mouse = document.getElementById('mouse');
-        mouse.style.opacity = 1;
-        mouse.style.transform = `translate(${element.clientX}px,${element.clientY}px) scale(${xscale},${yscale})`;
-        
-    });
-}
+// function mouseCircle(xscale,yscale){
+//     window.addEventListener('mousemove',(element)=>{
+//         let mouse = document.getElementById('mouse');
+//         mouse.style.opacity = 1;
+//         // mouse.style.transform = `translate(${element.clientX}px,${element.clientY}px) scale(${xscale},${yscale})`;
+//         mouse.style.cssText = `left: ${element.clientX}px; top: ${element.clientY}px;`;
+//     });
+// }
 
 // Function to implement the skewing animation of moving mouse circle 
-let timeout;
-function mouseCircleSkewer(){
-    let xprev = 0;
-    let yprev = 0;
-    window.addEventListener('mousemove',(event)=>{
-        clearTimeout(timeout);
-        xscale = gsap.utils.clamp(0.8,1.2,event.clientX-xprev);
-        yscale = gsap.utils.clamp(0.8,1.2,event.clientY-yprev);
-        xprev = event.clientX;
-        yprev = event.clientY;
-        mouseCircle(xscale,yscale);
-        timeout = setTimeout(() => {
-            let mouse = document.getElementById('mouse');
-            mouse.style.transform = `translate(${event.clientX}px,${event.clientY}px) scale(1,1)`;
-        }, 100);
-    });
-}
+// let timeout;
+// function mouseCircleSkewer(){
+//     let xprev = 0;
+//     let yprev = 0;
+//     window.addEventListener('mousemove',(event)=>{
+//         clearTimeout(timeout);
+//         xscale = gsap.utils.clamp(0.8,1.2,event.clientX-xprev);
+//         yscale = gsap.utils.clamp(0.8,1.2,event.clientY-yprev);
+//         xprev = event.clientX;
+//         yprev = event.clientY;
+//         mouseCircle(xscale,yscale);
+//         timeout = setTimeout(() => {
+//             let mouse = document.getElementById('mouse');
+//             mouse.style.transform = `translate(${event.clientX}px,${event.clientY}px) scale(1,1)`;
+//         }, 100);
+//     });
+// }
 
 // Function to implement the bottom to top moving text animation of the landing page
 function firstPageAnimation(){
@@ -58,9 +58,9 @@ function firstPageAnimation(){
     })
 }
 
-mouseCircle();
+// mouseCircle();
 firstPageAnimation();
-mouseCircleSkewer();
+// mouseCircleSkewer();
 
 // Fuctions to implement the hovering image animation on the skill section
 document.querySelectorAll("#skillbox").forEach((element)=>{
